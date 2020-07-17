77a2f529d65f50908c18723d69fc5238e4640f24
kgbhaumik86 <kgbhaumik86@gmail.com>
2020-07-17 13:00:42 +0530

    Updates

---
+++ precache-manifest.5958ab2939cea486f164d2ce4a562b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4dd9d8952ce124c04898d3179ef75cf",
+    "url": "/reactdeploy/index.html"
+  },
+  {
+    "revision": "7fe5b86a36629f98c1c1",
+    "url": "/reactdeploy/static/css/main.1a181726.chunk.css"
+  },
+  {
+    "revision": "bab144680513d9d09340",
+    "url": "/reactdeploy/static/js/2.38977539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdeploy/static/js/2.38977539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe5b86a36629f98c1c1",
+    "url": "/reactdeploy/static/js/main.ee778d32.chunk.js"
+  },
+  {
+    "revision": "6b2377b481f1800eb759",
+    "url": "/reactdeploy/static/js/runtime-main.ac38403b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
